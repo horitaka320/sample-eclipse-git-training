91b5e1b101976c7b008a948318e0fcddbc63e5d3
hori <horimain0705@gmail.com>
2026-05-27 17:25:51 +0900

    Initial commit for Eclipse Git training

---
+++ src/main/java/com/example/training/Main.java
@@ -0,0 +1,10 @@
+package com.example.training;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO: このメッセージを書き換える演習を行います
+        System.out.println("Hello, Git training with Eclipse!");
+        System.out.println("Please update this message on your feature branch.");
+    }
+}
